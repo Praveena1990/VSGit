01b9c85666d09a25e7f3e7c4f2b984c826460587
Praveena <Praveena.Soman@siemens-healthineers.com>
2020-02-24 15:10:15 +0530

    VSUpdate

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = addNumbers(10 , 12);
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+
+        private static int addNumbers(int v1 , int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
